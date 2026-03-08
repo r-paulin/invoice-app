44cc9b9826c2c4547ef0a75e8c794b2ee5193534
r-paulin <paulins.reinis@gmail.com>
2026-03-08 21:17:05 +0200

    update

--- assets/js/components/invoices-today-counter.js
+++ assets/js/components/invoices-today-counter.js
@@ -9,6 +9,7 @@
   var NEXT_INTERVAL_MS_MIN = 24000;
   var NEXT_INTERVAL_MS_MAX = 24000;
   var ANIMATION_DURATION_MS = 400;
+  var EVENT_NAME = 'invoicesToday:increment';
 
   function getTodayKey() {
     var d = new Date();
@@ -50,6 +51,41 @@
     return min + Math.floor(Math.random() * (max - min + 1));
   }
 
+  var chainStarted = false;
+
+  function doOneIncrement() {
+    var todayKey = getTodayKey();
+    var raw = sessionStorage.getItem(STORAGE_KEY);
+    var data = null;
+    try {
+      data = raw ? JSON.parse(raw) : null;
+    } catch (e) { /* ignore */ }
+    var value = (data && data.date === todayKey && typeof data.value === 'number')
+      ? data.value
+      : dailySeedFromDate(todayKey);
+    value += 1;
+    saveStored(todayKey, value);
+    window.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { value: value } }));
+  }
+
+  function scheduleNext() {
+    var delay = randomBetween(NEXT_INTERVAL_MS_MIN, NEXT_INTERVAL_MS_MAX);
+    setTimeout(function () {
+      doOneIncrement();
+      scheduleNext();
+    }, delay);
+  }
+
+  function startIncrementChain() {
+    if (chainStarted) return;
+    chainStarted = true;
+    var firstDelay = randomBetween(FIRST_INCREMENT_MS_MIN, FIRST_INCREMENT_MS_MAX);
+    setTimeout(function () {
+      doOneIncrement();
+      scheduleNext();
+    }, firstDelay);
+  }
+
   document.addEventListener('alpine:init', function () {
     Alpine.data('invoicesTodayCounter', function () {
       var todayKey = getTodayKey();
@@ -62,45 +98,28 @@
       return {
         count: initial,
         animating: false,
-        _timeouts: [],
 
         bump: function () {
-          var self = this;
+          var comp = this;
           this.animating = true;
           this.$nextTick(function () {
             setTimeout(function () {
-              self.animating = false;
+              comp.animating = false;
             }, ANIMATION_DURATION_MS);
           });
         },
 
-        increment: function () {
-          this.count += 1;
-          saveStored(getTodayKey(), this.count);
-          this.bump();
-        },
-
-        scheduleNext: function () {
-          var self = this;
-          var delay = randomBetween(NEXT_INTERVAL_MS_MIN, NEXT_INTERVAL_MS_MAX);
-          var t = setTimeout(function () {
-            self.increment();
-            self.scheduleNext();
-          }, delay);
-          this._timeouts.push(t);
-        },
-
         init: function () {
-          var self = this;
-          if (stored !== null) {
-            return;
+          var component = this;
+          function onIncrement(e) {
+            component.count = e.detail.value;
+            component.bump();
           }
-          var firstDelay = randomBetween(FIRST_INCREMENT_MS_MIN, FIRST_INCREMENT_MS_MAX);
-          var t = setTimeout(function () {
-            self.increment();
-            self.scheduleNext();
-          }, firstDelay);
-          this._timeouts.push(t);
+          document.addEventListener(EVENT_NAME, onIncrement);
+
+          if (stored === null) {
+            startIncrementChain();
+          }
         }
       };
     });
